030879b37de43e3dfbed9ccef30688636ff6bb76
sirimoota <sirimoota.123@gmail.com>
2025-11-20 19:05:34 +0530

    Update and rename db.py to db.js

--- Backend/app/db.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from .config import MONGO_URL, MONGO_DB
-
-client = AsyncIOMotorClient(MONGO_URL)
-db = client[MONGO_DB]
-
-# collections: posts, analytics
-posts_collection = db["posts"]
